902a40f460285b91c705acc9807902fda46855c7
Krieger251677 <69798247+Krieger251677@users.noreply.github.com>
2020-11-30 11:47:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+    /*    öğrenci sınıfı oluşturulacak, özellikler öğrenci No, ad, soyad, doğum yılı, bölüm 
+     *    (teknik bilimlerden olması lazım),
+          vize notu final notu ortalama ve karar(geçti kaldı) get set kullanılarak 
+          sınıf ve nesne oluşturulacak, 
+          nesnenin özellikleri ekrana yazdırıacak
+    */
+
+        static void Main(string[] args)
+        {
+            // Öğrenci bilgileri
+            ogrenci ogrenci = new ogrenci
+            {
+                no = 221903061,
+                ad = "Abdulkadir",
+                soyad = "Yeşilkaya",
+                dog_yıl = 2001,
+                bolum = "Bilgisayar Programcılığı",
+
+            };
+
+            ogrenci.ogrenciBilgileri();
+
+            ogrenci notlar = new ogrenci();
+
+            Console.WriteLine("Vize Notunuzu Giriniz. ");
+
+            notlar.vize = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Final Notunuzu Giriniz. ");
+            notlar.final = Convert.ToInt32(Console.ReadLine());
+            
+
+
+            Console.WriteLine("Notların Ortalaması => " + notlar.ortalama());
+
+            notlar.karar();
+
+        }
+    }
+}
---
+++ ogrenci.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ogrenci
+    {
+        public int no { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public int dog_yıl { get; set; }
+        public string bolum { get; set; }
+
+        public void ogrenciBilgileri()
+        {
+            Console.WriteLine("***********************************");
+            Console.WriteLine("Öğrenci Bilgileri");
+            Console.WriteLine("Numarası:   " + no);
+            Console.WriteLine("Adı:        " + ad);
+            Console.WriteLine("Soyadı:     " + soyad);
+            Console.WriteLine("Doğum Yılı: " + dog_yıl);
+            Console.WriteLine("Bölüm:      " + bolum);
+            Console.WriteLine("***********************************");
+
+        }
+
+
+        public int vize { get; set; }
+        public int final { get; set; }
+
+        public double ortalama()
+        {
+            double a = (vize * 0.4) + (final * 0.6);
+            return a;
+        }
+
+        public void karar()
+        {
+
+            if (ortalama() < 50)
+            {
+                Console.WriteLine("Kaldınız.");
+            }
+            else
+            {
+                Console.WriteLine("Geçtiniz.");
+            }
+
+        }
+
+    }
+}
